b466ec4b2afab3d725c4f5de23eb32961e124120
Marcos Jr <marcos_jrc@hotmail.com>
2018-10-30 10:39:51 -0400

    Atualizado com gitIgnore

--- src/main/java/com/marcos/cursomc/domain/Produto.java
+++ src/main/java/com/marcos/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
@@ -40,7 +39,7 @@ public class Produto  implements Serializable {
 	}
 	
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA",
 				joinColumns = @JoinColumn(name ="produto_id"),
