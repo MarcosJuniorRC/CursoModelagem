4394e56d7ba0fa249dc1bd7ffedf13291926a71a
Marcos Jr <marcos_jrc@hotmail.com>
2018-10-31 08:49:24 -0400

    Incluso validação no DTO da Categoria

--- src/main/java/com/marcos/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marcos.cursomc.domain.Categoria;
+import com.marcos.cursomc.dto.CategoriaDTO;
 import com.marcos.cursomc.repositores.CategoriaRepository;
 import com.marcos.cursomc.services.exceptions.DataIntegrityException;
 import com.marcos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria>findPage(Integer page,Integer linesPerPage,String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 	
 }
